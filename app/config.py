cb8e910800e7f9b20d3fe96fbef175f9092302ec
Abhijit Hambir <70660711+AbhijeetHambir02@users.noreply.github.com>
2025-11-23 23:23:27 +0530

    Update NEON_DB_URL for database connection

--- app/config.py
+++ app/config.py
@@ -1,1 +1,1 @@
-NEON_DB_URL = 'postgresql://neondb_owner:npg_Y8B0fSjapoty@ep-nameless-mountain-ahnkb20d-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+NEON_DB_URL = 'postgresql://neondb_owner:npg_RV5emBrF3iYq@ep-weathered-forest-ad6xenz7-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
